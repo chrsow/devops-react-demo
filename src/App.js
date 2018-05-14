105a3efb8c8fc90e7592e70799df9d9179370891
chrsow <liang_wasin@hotmail.co.th>
2018-05-14 10:25:22 +0700

    Update App.js on circleci

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <p> Hello Circle CI </p>
+        <p> Good bye circleci </p>
       </div>
     );
   }
