37236ab6d45d590c4a4e752e14fce62d66e221bc
chrsow <liang_wasin@hotmail.co.th>
2018-05-14 10:28:04 +0700

    Update netlify

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hello From Netlify</p>
+        <p>Goodbye From Netlify</p>
       </div>
     );
   }
